a33643086c15e59bf0ab56df621802064aa06bbd
Cathrin Senst <cathrin.senst@gmail.com>
2021-02-22 17:28:48 +0100

    replaced np functions by equivalent torch functions; added adjustment of brightness to augmentation

--- simclr/modules/transformations/simclr.py
+++ simclr/modules/transformations/simclr.py
@@ -1,5 +1,5 @@
-import numpy as np
 import torchvision
+import numpy as np
 import torch
 import torch.nn
 
@@ -46,6 +46,7 @@ class TransformsSimCLRAtari:
     def __init__(self, width, height, random_cropping):
         self.padding = torch.nn.ReplicationPad2d((12, 12, 16, 16))
         self.random_cropping = random_cropping
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else 'cpu')
         # if self.random_cropping:
         #     self.train_transform = torchvision.transforms.Compose(
         #         [
@@ -79,6 +80,7 @@ class TransformsSimCLRAtari:
         # )
 
     def __call__(self, x):
+        x = x.to(self.device)
         assert x.shape[0] == 4
 
         if torch.max(x) > 1.1:
@@ -89,31 +91,28 @@ class TransformsSimCLRAtari:
         else:
             sample1 = x
             sample2 = x.clone()
+
+        sample1, sample2 = self.adjust_brightness(sample1, sample2)
         sample1 = self.random_convolution(sample1)
         sample2 = self.random_convolution(sample2)
 
-        # if torch.max(sample1[0, :, :]) > 1.4:
-        #     sample1_prev = sample1.clone()
-        # else:
-        #     sample1_prev = None
         sample1 = torch.clamp(sample1, 0, 1.)
         sample2 = torch.clamp(sample2, 0, 1.)
 
 ### for plotting / debugging
-        # if sample1_prev is not None:
-        #     im_sample1 = sample1.squeeze()
-        #     im1 = im_sample1[0, :, :]
-        #     im_sample1_prev = sample1_prev.squeeze()
-        #     im2 = im_sample1_prev[0, :, :]
-        #     for i in range(1, 4):
-        #         # 3, frameestack, height, width
-        #         im1 = torch.cat((im1, im_sample1[i, :, :]), axis=-1)  # via the last axis --> width
-        #         im2 = torch.cat((im2, im_sample1_prev[i, :, :]), axis=-1)  # via the last axis --> width
-        #     im1 = torchvision.transforms.ToPILImage()(im1)
-        #     im2 = torchvision.transforms.ToPILImage()(im2)
-        #     random_int = np.random.randint(20)
-        #     im1.save(f'/home/cathrin/MA/datadump/simclr/'+str(random_int) + '_1.png')
-        #     im2.save(f'/home/cathrin/MA/datadump/simclr/'+str(random_int) + '_2.png')
+        # im_sample1 = sample1.squeeze()
+        # im1 = im_sample1[0, :, :]
+        # im_sample2 = sample2.squeeze()
+        # im2 = im_sample2[0, :, :]
+        # for i in range(1, 4):
+        #     # 3, frameestack, height, width
+        #     im1 = torch.cat((im1, im_sample1[i, :, :]), axis=-1)  # via the last axis --> width
+        #     im2 = torch.cat((im2, im_sample2[i, :, :]), axis=-1)  # via the last axis --> width
+        # im1 = torchvision.transforms.ToPILImage()(im1)
+        # im2 = torchvision.transforms.ToPILImage()(im2)
+        # random_int = np.random.randint(20)
+        # im1.save(f'/home/cathrin/MA/datadump/simclr/'+str(random_int) + '_1.png')
+        # im2.save(f'/home/cathrin/MA/datadump/simclr/'+str(random_int) + '_2.png')
         return sample1, sample2
 
     def first_pad_then_random_crop(self, img_stack):
@@ -122,8 +121,8 @@ class TransformsSimCLRAtari:
         obs_padded = self.padding(img_stack_dim4).squeeze(0) # dim 3 again
         c, h, w = obs_padded.shape
         # generate random int between 0 and (w-160+1)-1  !
-        w_shift = np.random.randint(w - 160 + 1, size=2)
-        h_shift = np.random.randint(h - 210 + 1, size=2)
+        w_shift = torch.randint(w - 160 + 1, size=(2,))
+        h_shift = torch.randint(h - 210 + 1, size=(2,))
         img_stack1 = obs_padded[:, h_shift[0]:(h_shift[0]+210), w_shift[0]:(w_shift[0]+160)]
         img_stack2 = obs_padded[:, h_shift[1]:(h_shift[1]+210), w_shift[1]:(w_shift[1]+160)]
         assert img_stack1.shape == img_stack2.shape == (4, 210, 160), f"shape is {img_stack2.shape}, input shape was: {obs_padded.shape} w: {w_shift[1]}, h: {h_shift[1]}"
@@ -139,13 +138,12 @@ class TransformsSimCLRAtari:
         '''
         assert len(imgs.shape) == 3
         assert imgs.shape[0] == 4
-        _device = imgs.device
 
         img_h, img_w = imgs.shape[-2], imgs.shape[-1]
         num_stack_channel = imgs.shape[-3]
 
         # initialize random covolution
-        rand_conv = torch.nn.Conv2d(1, 1, kernel_size=3, bias=False, padding=1).to(_device).requires_grad_(False)
+        rand_conv = torch.nn.Conv2d(1, 1, kernel_size=3, bias=False, padding=1).to(self.device).requires_grad_(False)
 
         torch.nn.init.xavier_normal_(rand_conv.weight.data)
         for i in range(num_stack_channel):
@@ -159,3 +157,7 @@ class TransformsSimCLRAtari:
                 total_out = torch.cat((total_out, rand_out), 0)
         total_out = total_out.reshape(num_stack_channel, img_h, img_w)
         return total_out.squeeze()
+
+    def adjust_brightness(self, x1, x2):
+        brightness_fct = torch.FloatTensor(2,).uniform_(0.92, 1.08)
+        return torchvision.transforms.functional.adjust_brightness(x1, brightness_factor=brightness_fct[0]), torchvision.transforms.functional.adjust_brightness(x2, brightness_factor=brightness_fct[1])
