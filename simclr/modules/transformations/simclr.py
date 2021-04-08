3e95d176d72d8b05b65ec3d24ea6d6dae93c99f6
Cathrin Senst <cathrin.senst@gmail.com>
2021-04-08 10:24:03 +0200

    removed unnecessary to-moving process (otherwise cuda spawning related errors)

--- simclr/modules/transformations/simclr.py
+++ simclr/modules/transformations/simclr.py
@@ -1,8 +1,9 @@
 import kornia
+import numpy as np
 import torchvision
 import torch
 import torch.nn
-import numpy as np
+
 from termcolor import colored
 
 
@@ -254,7 +255,7 @@ class TransformsSimCLRAtari:
         # )
 
     def __call__(self, x):
-        x = x.to(self.device)
+        # x = x.to(self.device)
         assert x.shape[0] == 4
 
         if torch.max(x) > 1.1:
